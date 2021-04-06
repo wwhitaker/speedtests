d8dfbbbcab68497c7a3dca97bf43b35329cc7a7a
davidtaddei <YOUR_EMAIL_ADDRESS_HERE@users.noreply.github.com>
2021-04-06 21:49:48 +0100

    feat: Updated to influxDB v2

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This is a Python script that will continuously run the official Speedtest CLI application by Ookla, takes input from environment variables, formats data and writes it to an InfluxDB database.
 
-This script will allow you to measure your internet connections speed and consistency over time. It uses env variables as configuration. It's as easy to use as telling your Docker server a 1 line command and you'll be set. Using Grafana you can start exploring this data easily. 
+This script will allow you to measure your internet connections speed and consistency over time. It uses env variables as configuration. It's as easy to use as telling your Docker server a 1 line command and you'll be set. Using Grafana you can start exploring this data easily.
 
 I built a Grafana dashboard which has been exported into this repo as `GrafanaDash-SpeedTests.json` to import into Grafana for your convenience.
 
 ![GrafanaDashboard](https://user-images.githubusercontent.com/945191/105287048-46f52a80-5b6c-11eb-9e57-038d63b67efb.png)
 
-There are some added features to allow some additional details that Ookla provides as tags on your data. Some examples are your current ISP, the interface being used, the server who hosted the test. Overtime, you could identify if some serers are performing better than others. 
+There are some added features to allow some additional details that Ookla provides as tags on your data. Some examples are your current ISP, the interface being used, the server who hosted the test. Overtime, you could identify if some serers are performing better than others.
 
 ## Configuring the script
 
@@ -29,12 +29,12 @@ The variables available are:
 
 ### Variable Notes
 - Intervals are in minutes. *Script will convert it to seconds.*
-- If any variables are not needed, don't declare them. Functions will operate with or without most variables. 
+- If any variables are not needed, don't declare them. Functions will operate with or without most variables.
 - Tags should be input without quotes. *INFLUX_DB_TAGS = isp, interface, external_ip, server_name, speedtest_url*
 - NAMESPACE is used to collect data from multiple instances of the container into one database and select which you wish to view in Grafana. i.e. I have one monitoring my Starlink, the other my TELUS connection.
-  
+
 ### Tag Options
-The Ookla speedtest app provides a nice set of data beyond the upload and download speed. The list is below. 
+The Ookla speedtest app provides a nice set of data beyond the upload and download speed. The list is below.
 
 | Tag Name 	| Description 	|
 |-	|-	|
@@ -59,7 +59,7 @@ Be aware that this script will automatically accept the license and GDPR stateme
 
 ## Running the Script
 
-### Ideal option, run as a Docker container. 
+### Ideal option, run as a Docker container.
 
 1. Build the container.
 
--- main.py
+++ main.py
@@ -1,50 +1,55 @@
-import os
-import time
-import json
 import datetime
+import json
+import os
 import subprocess
-from pythonping import ping
-from influxdb import InfluxDBClient
+import time
 from multiprocessing import Process
 
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from pythonping import ping
+
 # InfluxDB Settings
-NAMESPACE = os.getenv('NAMESPACE', 'None')
-DB_ADDRESS = os.getenv('INFLUX_DB_ADDRESS', 'influxdb')
-DB_PORT = int(os.getenv('INFLUX_DB_PORT', '8086'))
-DB_USER = os.getenv('INFLUX_DB_USER', '')
-DB_PASSWORD = os.getenv('INFLUX_DB_PASSWORD', '')
-DB_DATABASE = os.getenv('INFLUX_DB_DATABASE', 'speedtests')
-DB_TAGS = os.getenv('INFLUX_DB_TAGS', None)
-PING_TARGETS = os.getenv('PING_TARGETS', '1.1.1.1, 8.8.8.8')
+NAMESPACE = os.getenv("NAMESPACE", "None")
+DB_ADDRESS = os.getenv("INFLUX_DB_ADDRESS", "influxdb")
+DB_PORT = int(os.getenv("INFLUX_DB_PORT", "8086"))
+DB_ORG = os.getenv("INFLUX_DB_ORG", "")
+DB_TOKEN = os.getenv("INFLUX_DB_TOKEN", "")
+DB_DATABASE = os.getenv("INFLUX_DB_DATABASE", "speedtests")
+DB_TAGS = os.getenv("INFLUX_DB_TAGS", None)
+PING_TARGETS = os.getenv("PING_TARGETS", "1.1.1.1, 8.8.8.8")
 
 # Speedtest Settings
 # Time between tests (in minutes, converts to seconds).
-TEST_INTERVAL = int(os.getenv('SPEEDTEST_INTERVAL', '5')) * 60
+TEST_INTERVAL = int(os.getenv("SPEEDTEST_INTERVAL", "5")) * 60
 # Time before retrying a failed Speedtest (in minutes, converts to seconds).
-TEST_FAIL_INTERVAL = int(os.getenv('SPEEDTEST_FAIL_INTERVAL', '5')) * 60
+TEST_FAIL_INTERVAL = int(os.getenv("SPEEDTEST_FAIL_INTERVAL", "5")) * 60
 # Specific server ID
-SERVER_ID = os.getenv('SPEEDTEST_SERVER_ID', '')
+SERVER_ID = os.getenv("SPEEDTEST_SERVER_ID", "")
 # Time between ping tests (in seconds).
-PING_INTERVAL = int(os.getenv('PING_INTERVAL', '5'))
+PING_INTERVAL = int(os.getenv("PING_INTERVAL", "5"))
 
-influxdb_client = InfluxDBClient(
-    DB_ADDRESS, DB_PORT, DB_USER, DB_PASSWORD, None)
+client = InfluxDBClient(
+    url=f"{DB_ADDRESS}:{DB_PORT}", token=f"{DB_TOKEN}", org=f"{DB_ORG}"
+)
 
+write_api = client.write_api(write_options=SYNCHRONOUS)
+query_api = client.query_api()
 
-def init_db():
-    databases = influxdb_client.get_list_database()
+# def init_db():
+#    databases = query_api.get_list_database()
 
-    if len(list(filter(lambda x: x['name'] == DB_DATABASE, databases))) == 0:
-        influxdb_client.create_database(
-            DB_DATABASE)  # Create if does not exist.
-    else:
-        # Switch to if does exist.
-        influxdb_client.switch_database(DB_DATABASE)
+#    if len(list(filter(lambda x: x['name'] == DB_DATABASE, databases))) == 0:
+#        influxdb_client.create_database(
+#            DB_DATABASE)  # Create if does not exist.
+#    else:
+# Switch to if does exist.
+#        influxdb_client.switch_database(DB_DATABASE)
 
 
 def pkt_loss(data):
-    if 'packetLoss' in data.keys():
-        return int(data['packetLoss'])
+    if "packetLoss" in data.keys():
+        return int(data["packetLoss"])
     else:
         return 0
 
@@ -55,32 +60,32 @@ def tag_selection(data):
 
     # tag_switch takes in _data and attaches CLIoutput to more readable ids
     tag_switch = {
-        'namespace': NAMESPACE,
-        'isp': data['isp'],
-        'interface': data['interface']['name'],
-        'internal_ip': data['interface']['internalIp'],
-        'interface_mac': data['interface']['macAddr'],
-        'vpn_enabled': (False if data['interface']['isVpn'] == 'false' else True),
-        'external_ip': data['interface']['externalIp'],
-        'server_id': data['server']['id'],
-        'server_name': data['server']['name'],
-        'server_location': data['server']['location'],
-        'server_country': data['server']['country'],
-        'server_host': data['server']['host'],
-        'server_port': data['server']['port'],
-        'server_ip': data['server']['ip'],
-        'speedtest_id': data['result']['id'],
-        'speedtest_url': data['result']['url']
+        "namespace": NAMESPACE,
+        "isp": data["isp"],
+        "interface": data["interface"]["name"],
+        "internal_ip": data["interface"]["internalIp"],
+        "interface_mac": data["interface"]["macAddr"],
+        "vpn_enabled": (False if data["interface"]["isVpn"] == "false" else True),
+        "external_ip": data["interface"]["externalIp"],
+        "server_id": data["server"]["id"],
+        "server_name": data["server"]["name"],
+        "server_location": data["server"]["location"],
+        "server_country": data["server"]["country"],
+        "server_host": data["server"]["host"],
+        "server_port": data["server"]["port"],
+        "server_ip": data["server"]["ip"],
+        "speedtest_id": data["result"]["id"],
+        "speedtest_url": data["result"]["url"],
     }
 
     if tags is None:
-        tags = 'namespace'
-    elif '*' in tags:
+        tags = "namespace"
+    elif "*" in tags:
         return tag_switch
     else:
-        tags = 'namespace, ' + tags
+        tags = "namespace, " + tags
 
-    tags = tags.split(',')
+    tags = tags.split(",")
     for tag in tags:
         # split the tag string, strip and add selected tags to {options} with corresponding tag_switch data
         tag = tag.strip()
@@ -93,63 +98,41 @@ def format_for_influx(data):
 
     # There is additional data in the speedtest-cli output but it is likely not necessary to store.
     influx_data = [
-        {
-            'measurement': 'ping',
-            'time': data['timestamp'],
-            'fields': {
-                'jitter': data['ping']['jitter'],
-                'latency': data['ping']['latency']
-            }
-        },
-        {
-            'measurement': 'download',
-            'time': data['timestamp'],
-            'fields': {
-                # Byte to Megabit
-                'bandwidth': data['download']['bandwidth'] / 125000,
-                'bytes': data['download']['bytes'],
-                'elapsed': data['download']['elapsed']
-            }
-        },
-        {
-            'measurement': 'upload',
-            'time': data['timestamp'],
-            'fields': {
-                # Byte to Megabit
-                'bandwidth': data['upload']['bandwidth'] / 125000,
-                'bytes': data['upload']['bytes'],
-                'elapsed': data['upload']['elapsed']
-            }
-        },
-        {
-            'measurement': 'packetLoss',
-            'time': data['timestamp'],
-            'fields': {
-                'packetLoss': pkt_loss(data)
-            }
-        },
-        {
-            'measurement': 'speeds',
-            'time': data['timestamp'],
-            'fields': {
-                'jitter': data['ping']['jitter'],
-                'latency': data['ping']['latency'],
-                'packetLoss': pkt_loss(data),
-                # Byte to Megabit
-                'bandwidth_down': data['download']['bandwidth'] / 125000,
-                'bytes_down': data['download']['bytes'],
-                'elapsed_down': data['download']['elapsed'],
-                # Byte to Megabit
-                'bandwidth_up': data['upload']['bandwidth'] / 125000,
-                'bytes_up': data['upload']['bytes'],
-                'elapsed_up': data['upload']['elapsed']
-            }
-        }
+        Point("ping")
+        .field("jitter", data["ping"]["jitter"])
+        .field("latency", data["ping"]["latency"])
+        .time(data["timestamp"], WritePrecision.MS),
+        Point("download")
+        .field("bandwidth", (data["download"]["bandwidth"] / 125000))
+        .field("bytes", data["download"]["bytes"])
+        .field("elapsed", data["download"]["elapsed"])
+        .time(data["timestamp"], WritePrecision.MS),
+        Point("upload")
+        .field("bandwidth", (data["upload"]["bandwidth"] / 125000))
+        .field("bytes", data["upload"]["bytes"])
+        .field("elapsed", data["upload"]["elapsed"])
+        .time(data["timestamp"], WritePrecision.MS),
+        Point("packetloss")
+        .field("packetLoss", pkt_loss(data))
+        .time(data["timestamp"], WritePrecision.MS),
+        Point("speeds")
+        .field("jitter", data["ping"]["jitter"])
+        .field("latency", data["ping"]["latency"])
+        .field("packetLoss", pkt_loss(data))
+        .field("bandwidth_down", (data["download"]["bandwidth"] / 125000))
+        .field("bytes_down", data["download"]["bytes"])
+        .field("elapsed_down", data["download"]["elapsed"])
+        .field("bandwidth_up", (data["upload"]["bandwidth"] / 125000))
+        .field("bytes_up", data["upload"]["bytes"])
+        .field("elapsed_up", data["upload"]["elapsed"])
+        .time(data["timestamp"], WritePrecision.MS),
     ]
+
     tags = tag_selection(data)
     if tags is not None:
         for measurement in influx_data:
-            measurement['tags'] = tags
+            for k, v in tags.items():
+                measurement.tag(k, v)
 
     return influx_data
 
@@ -157,59 +140,90 @@ def format_for_influx(data):
 def speedtest():
     if not SERVER_ID:
         speedtest = subprocess.run(
-        ["speedtest", "--accept-license", "--accept-gdpr", "-f", "json"], capture_output=True)
+            ["speedtest", "--accept-license", "--accept-gdpr", "-f", "json"],
+            capture_output=True,
+        )
         print("Automatic server choice")
     else:
         speedtest = subprocess.run(
-        ["speedtest", "--accept-license", "--accept-gdpr", "-f", "json", "--server-id=" + SERVER_ID], capture_output=True)
+            [
+                "speedtest",
+                "--accept-license",
+                "--accept-gdpr",
+                "-f",
+                "json",
+                "--server-id=" + SERVER_ID,
+            ],
+            capture_output=True,
+        )
         print("Manual server choice : ID = " + SERVER_ID)
 
     if speedtest.returncode == 0:  # Speedtest was successful.
         print("Speedtest Successful :")
         data_json = json.loads(speedtest.stdout)
-        print("time: " + str(data_json['timestamp']) + " - ping: " + str(data_json['ping']['latency']) + " ms - download: " + str(data_json['download']['bandwidth']/125000) + " Mb/s - upload: " + str(data_json['upload']['bandwidth'] / 125000) + " Mb/s - isp: " + data_json['isp'] + " - ext. IP: " + data_json['interface']['externalIp'] + " - server id: " + str(data_json['server']['id']) + " (" + data_json['server']['name'] + " @ " + data_json['server']['location'] + ")")
+        print(
+            "time: "
+            + str(data_json["timestamp"])
+            + " - ping: "
+            + str(data_json["ping"]["latency"])
+            + " ms - download: "
+            + str(data_json["download"]["bandwidth"] / 125000)
+            + " Mb/s - upload: "
+            + str(data_json["upload"]["bandwidth"] / 125000)
+            + " Mb/s - isp: "
+            + data_json["isp"]
+            + " - ext. IP: "
+            + data_json["interface"]["externalIp"]
+            + " - server id: "
+            + str(data_json["server"]["id"])
+            + " ("
+            + data_json["server"]["name"]
+            + " @ "
+            + data_json["server"]["location"]
+            + ")"
+        )
         data = format_for_influx(data_json)
-        if influxdb_client.write_points(data) == True:
-            print("Data written to DB successfully")
+        write_api.write(bucket="speedtests", record=data)
+        print("Data written to DB successfully")
     else:  # Speedtest failed.
         print("Speedtest Failed :")
         print(speedtest.stderr)
         print(speedtest.stdout)
+
+
 #        time.sleep(TEST_FAIL_INTERVAL)
 
 
 def pingtest():
     timestamp = datetime.datetime.utcnow()
-    for target in PING_TARGETS.split(','):
+    for target in PING_TARGETS.split(","):
         target = target.strip()
         pingtest = ping(target, verbose=False, timeout=1, count=1, size=128)
-        data = [
-            {
-                'measurement': 'pings',
-                'time': timestamp,
-                'tags': {
-                    'namespace': NAMESPACE,
-                    'target' : target
-                },
-                'fields': {
-                    'success' : int(pingtest._responses[0].error_message is None),
-                    'rtt': float(0 if pingtest._responses[0].error_message is not None else pingtest.rtt_avg_ms)
-                }
-            }
-        ]
-        if influxdb_client.write_points(data) == True:
-            print("Ping data written to DB successfully")
-        else:  # Speedtest failed.
-            print("Ping Failed.")
+
+        success = int(pingtest._responses[0].error_message is None)
+        rtt = float(
+            0
+            if pingtest._responses[0].error_message is not None
+            else pingtest.rtt_avg_ms
+        )
+        data = (
+            Point("pings")
+            .field("success", success)
+            .field("rtt", rtt)
+            .tag("namespace", NAMESPACE)
+            .tag("target", target)
+        )
+        write_api.write(bucket="speedtests", record=data)
+
 
 def main():
     pPing = Process(target=pingtest)
     pSpeed = Process(target=speedtest)
 
-    init_db()  # Setup the database if it does not already exist.
+    # init_db()  # Setup the database if it does not already exist.
 
     loopcount = 0
-    while (1):  # Run a Speedtest and send the results to influxDB indefinitely.
+    while 1:  # Run a Speedtest and send the results to influxDB indefinitely.
         if loopcount == 0 or loopcount % PING_INTERVAL == 0:
             if pPing.is_alive():
                 pPing.terminate()
@@ -222,12 +236,13 @@ def main():
             pSpeed = Process(target=speedtest)
             pSpeed.start()
 
-        if loopcount % ( PING_INTERVAL * TEST_INTERVAL ) == 0:
+        if loopcount % (PING_INTERVAL * TEST_INTERVAL) == 0:
             loopcount = 0
 
         time.sleep(1)
         loopcount += 1
 
-if __name__ == '__main__':
-    print('Speedtest CLI data logger to InfluxDB started...')
+
+if __name__ == "__main__":
+    print("Speedtest CLI data logger to InfluxDB started...")
     main()
